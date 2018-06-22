126536da2483db0a529cfae79adcda70cfe25c5b
GloryPassarello <gloria.passarello@virgilio.it>
2018-06-22 06:44:56 -0400

    changed script for plotting fishing data

--- code/make_map.R
+++ code/make_map.R
@@ -6,21 +6,41 @@
 
 
 #connectiong to the database
-make_map <- function(){
-  require(DBI)
-  library(RPostgreSQL)
-  library(rpostgis)
-  library(rgdal)
+  if(!require(DBI)){
+    install.packages("DBI")
+    library(DBI)
+  }
+  
+  if(!require(RPostgreSQL)){
+    install.packages("RPostgreSQL")
+    library(RPostgreSQL)
+  }
+  
+  if(!require(rpostgis)){
+    install.packages("rpostgis")
+    library(rpostgis)
+  }
+  
+  if(!require(rgdal)){
+    install.packages("rgdal")
+    library(rgdal)
+  }
+
+  if (!require('devtools')) install.packages('devtools')
+  devtools::install_github('rstudio/leaflet')
+  
+  
   drv <- dbDriver("PostgreSQL")
-  con <- dbConnect(drv, host = 'localhost', dbname = 'cs2_pressures', user = 'postgres', password = 'admin')
-  basin_mean <- pgGetGeom(conn = con, c("public", "basin_test_mean"))
-  basin_mean <- spTransform(basin_mean, CRS("+proj=longlat +datum=WGS84"))
+  con <- dbConnect(drv, host = 'localhost', dbname = 'fishing', user = 'postgres', password = 'admin')
+  fishing_mean <- pgGetGeom(conn = con, c("public", "2016-09-22_clip"))
+  fishing_mean <- spTransform(fishing_mean, CRS("+proj=longlat +datum=WGS84"))
   m <- leaflet::leaflet()
   m <- leaflet::addTiles(m)
-  m <- leaflet::addPolygons(m, data =basin_mean, color = "#444444", weight = 1, smoothFactor = 0.5, opacity = 1.0, fillOpacity = 0.5,popup = title)
+  m <- leaflet(data = fishing_mean )%>% addTiles() %>% addCircles(m, lng = fishing_mean$lon, lat = fishing_mean$lat, radius = fishing_mean$vessel_hou * 30  , layerId = NULL,
+                           group = NULL, stroke = TRUE, color = "#03F", weight = 1,
+                           opacity = 0.5)
   htmlwidgets::saveWidget(m, "mymap.html", selfcontained = FALSE)
-}
-
+  m
 
 
 
